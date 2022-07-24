4fc57c59ba45f25e69636161617c8428ab99f64a
BTAkademimcsd <53770975+BTAkademimcsd@users.noreply.github.com>
2022-07-24 10:29:56 +0300

    projeye main metodu eklendi...

---
+++ _7_Interface/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _7_Interface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Interface (Arayüz)
+            // Interface konusu inheritance ve abstraction konulanırı içerir...
+
+
+            //Class => kullanıcı tanımlı tipler oluşturmak için kullanılır
+            // Interface => kullanıcı tanımlı tipler oluşturmak için kullanılır..
+
+            // Interfaceler classlara yetenek kazandırmak için kullanılır;
+            // C# dilinde class sadece tek bir classtan miras alabilir...bu durumda multiple inheritance (çoklu miras) alınmak istendiğinde interface kullanılır. çünkü bir class birden fazla interface'den miras alabilir...
+            // 
+        }
+    }
+
+    public interface A
+    {
+
+    }
+    public interface B
+    {
+
+    }
+    public class C : A, B
+    {
+    }
+}
